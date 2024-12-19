34e80915147bc691aacff82660e8d3e595e0ac8e
Daksh <118450155+daksh1205@users.noreply.github.com>
2024-12-19 21:52:17 +0530

    feat: supabase setup

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://hjojpzaeoswpaigybjmx.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhqb2pwemFlb3N3cGFpZ3liam14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2MjQ4ODgsImV4cCI6MjA1MDIwMDg4OH0.uAeynWP8zJ0Z6qwyD8FXRgSPoIMGILf6DCDTiI_mtEU';
+}
